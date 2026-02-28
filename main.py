2611f297bd7a2983e518170b2dfb839f0c2d6a9a
daiyouyu <203870767@qq.com>
2026-02-28 09:18:13 +0800

    feat:添加结果时间显示

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+import os,time
 # 导入算法
 from flcore.train.train_ddpg import train_ddpg
 from flcore.train.train_iddpg import train_iddpg
@@ -11,10 +11,10 @@ from flcore.train.train_maddpg import train_maddpg
 
 if __name__ == "__main__":
     # re1,test_re = train_maddpg(episodes=100,train = 7  , test = 1 ,Federated = True)
-    iddpg, test_re = train_iddpg(episodes=1000, train=365, test=0, Federated=False)
-    Fed_iddpg, test_re = train_iddpg(episodes=1000, train=365, test=0, Federated=True)
-    maddpg, test_re = train_maddpg(episodes=1000, train=365, test=0, Federated=False)
-    Fed_maddpg, test_re = train_maddpg(episodes=1000, train=365, test=0, Federated=True)
+    iddpg, test_re = train_iddpg(episodes=1, train=365, test=0, Federated=False)
+    Fed_iddpg, test_re = train_iddpg(episodes=1, train=365, test=0, Federated=True)
+    maddpg, test_re = train_maddpg(episodes=1, train=365, test=0, Federated=False)
+    Fed_maddpg, test_re = train_maddpg(episodes=1, train=365, test=0, Federated=True)
 
     re1 = np.array(iddpg).T
     re2 = np.array(Fed_iddpg).T
@@ -25,15 +25,18 @@ if __name__ == "__main__":
     maddpg = np.array(re3)
 
     Fed_maddpg = np.array(re4)
+    now = time.time()
+    now = time.localtime(now)
+    now = time.strftime('%Y-%m-%d_%H:%M:%S', now)
     # 指定保存目录（例如当前目录下的 'data' 文件夹）
-    save_dir = './result'
+    save_dir = f'./result_{now}'
 
     # 检查目录是否存在，不存在则创建
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)  # 递归创建目录（包括父目录）
 
     # 拼接完整路径（目录 + 文件名）
-    file_path = os.path.join(save_dir, 'result_arrays')
+    file_path = os.path.join(save_dir, f'result_arrays')
 
     # 保存到指定目录
     np.savez(file_path, iddpg=iddpg, Fed_iddpg=Fed_iddpg, maddpg=maddpg, Fed_maddpg=Fed_maddpg)
