1852d8e7273cef20b728f3af27dd52acb0923955
daiyouyu <203870767@qq.com>
2026-04-03 15:20:51 +0800

    feat:完善平均联邦聚合

--- flcore/algorithm/IDDPG.py
+++ flcore/algorithm/IDDPG.py
@@ -243,7 +243,7 @@ class IDDPG:
                 Federated_w.append(w)
 
             # 修改：只返回每个代理在聚合中对自己的权重分配 (即对角线元素)
-            Federated_w_return = np.array([Federated_w[i][i] for i in range(self.n_agents)])
+            Federated_w_return = np.array(Federated_w)
 
             # 4) 依据权重做参数聚合，分别得到每个 agent 的新 actor
             with torch.no_grad():
